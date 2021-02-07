9ab5269ab10738397b82745b97e36d509cfb27a0
Asuka <yanghang8612@163.com>
2021-02-07 12:15:29 +0800

    test: find out_of_time usdt transfer

--- framework/src/main/java/org/tron/program/FullNode.java
+++ framework/src/main/java/org/tron/program/FullNode.java
@@ -5,20 +5,21 @@ import ch.qos.logback.classic.joran.JoranConfigurator;
 import java.io.File;
 import java.util.List;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.tron.common.application.Application;
 import org.tron.common.application.ApplicationFactory;
 import org.tron.common.application.TronApplicationContext;
+import org.tron.common.crypto.Hash;
 import org.tron.common.parameter.CommonParameter;
+import org.tron.common.utils.StringUtil;
 import org.tron.core.Constant;
 import org.tron.core.capsule.BlockCapsule;
 import org.tron.core.capsule.TransactionCapsule;
 import org.tron.core.config.DefaultConfig;
 import org.tron.core.config.args.Args;
-import org.tron.core.db.BlockStore;
-import org.tron.core.db.Manager;
 import org.tron.core.services.RpcApiService;
 import org.tron.core.services.http.FullNodeHttpApiService;
 import org.tron.core.services.interfaceOnPBFT.RpcApiServiceOnPBFT;
@@ -29,6 +30,7 @@ import org.tron.core.store.StoreFactory;
 import org.tron.core.vm.repository.Repository;
 import org.tron.core.vm.repository.RepositoryImpl;
 import org.tron.protos.Protocol;
+import org.tron.protos.contract.SmartContractOuterClass;
 
 @Slf4j(topic = "app")
 public class FullNode {
@@ -121,19 +123,47 @@ public class FullNode {
 
     new Thread(() -> {
       Repository repository = RepositoryImpl.createRoot(StoreFactory.getInstance());
+      byte[] selector = Hash.sha3("transfer(address,uint256)".getBytes());
+      long txCnt = 0, outOfTime = 0;
       for (int i = 0; i < 10512000; i++) {
+        if (i % 100000 == 1) {
+          System.out.println("total: " + txCnt + ", outoftime: " + outOfTime);
+        }
         long num = 27328000;
         BlockCapsule blockCapsule = repository.getBlockByNum(num - i);
         List<TransactionCapsule> transactions = blockCapsule.getTransactions();
-        for (TransactionCapsule t : transactions) {
-          if (t.getContractResult() == Protocol.Transaction.Result.contractResult.OUT_OF_TIME) {
-
+        for (TransactionCapsule cap : transactions) {
+          Protocol.Transaction t = cap.getInstance();
+          List<Protocol.Transaction.Contract> contracts = t.getRawData().getContractList();
+          if (contracts.size() > 0 && contracts.get(0).getType()
+              == Protocol.Transaction.Contract.ContractType.TriggerSmartContract) {
+            try {
+              SmartContractOuterClass.TriggerSmartContract contract =
+                  contracts.get(0).getParameter().unpack(SmartContractOuterClass.TriggerSmartContract.class);
+              if ("TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t".equals(StringUtil.encode58Check(contract.getContractAddress().toByteArray()))
+                  && check(selector, contract.getData().toByteArray())) {
+                txCnt += 1;
+                if (cap.getContractResult() == Protocol.Transaction.Result.contractResult.OUT_OF_TIME) {
+                  outOfTime += 1;
+                  System.out.println(cap.getTransactionId().toString());
+                }
+              }
+            } catch (InvalidProtocolBufferException e) {
+              e.printStackTrace();
+            }
           }
         }
       }
     }).start();
 
     rpcApiService.blockUntilShutdown();
+  }
+
+  private static boolean check(byte[] a, byte[] b) {
+    for (int i = 0; i < 4; i++) {
+      if (a[i] != b[i]) return false;
+    }
+    return true;
   }
 
   public static void shutdown(final Application app) {
