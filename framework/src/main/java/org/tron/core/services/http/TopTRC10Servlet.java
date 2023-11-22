ee0a0711e9d76bee2d57cdc31a8e54bb93f6d84d
Asuka <yanghang8612@163.com>
2023-11-22 11:17:52 +0800

    func(stats): better output

--- framework/src/main/java/org/tron/core/services/http/TopTRC10Servlet.java
+++ framework/src/main/java/org/tron/core/services/http/TopTRC10Servlet.java
@@ -10,10 +10,7 @@ import org.tron.core.store.DynamicPropertiesStore;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 @Slf4j(topic = "API")
@@ -30,23 +27,25 @@ public class TopTRC10Servlet extends RateLimiterServlet {
       long cycleCount = request.getParameter("cycle_count") == null ?
               1 : Long.parseLong(request.getParameter("cycle_count"));
 
-      Map<WrappedByteArray, Long> result = new HashMap<>();
+      Map<String, Long> result = new HashMap<>();
       for (int i = 0; i < cycleCount; i++) {
         byte[] prefix = ((cycleNumber + i) + "-").getBytes();
         byte[] key = new byte[prefix.length + 1];
         System.arraycopy(prefix, 0, key, 0, prefix.length);
         key[prefix.length] = 0x51;
         Map<WrappedByteArray, ContractStateCapsule> data = css.prefixQuery(key);
-        data.forEach((k, v) -> result.put(k, result.getOrDefault(k, 0L) + v.getTxTrc10Count()));
+        data.forEach((k, v) -> {
+          String tokeName = new String(Arrays.copyOfRange(k.getBytes(), key.length, k.getBytes().length - 1));
+          result.put(tokeName, result.getOrDefault(tokeName, 0L) + v.getTxTrc10Count());
+        });
       }
 
-      List<Map.Entry<WrappedByteArray, Long>> list = new LinkedList<>(result.entrySet());
+      List<Map.Entry<String, Long>> list = new LinkedList<>(result.entrySet());
       list.sort((o1, o2) -> Long.compare(o2.getValue(), o1.getValue()));
 
-      response.getWriter().println("\nTop 20 TRC10:\n");
+      response.getWriter().println("Top 20 Transfer TRC10:\n");
       for (int i = 0; i < 20; i++) {
-        response.getWriter().println(new String(list.get(i).getKey().getBytes()) +
-                ": " + list.get(i).getValue());
+        response.getWriter().println(list.get(i).getKey() + ": " + list.get(i).getValue());
       }
     } catch (Exception e) {
       Util.processError(e, response);
