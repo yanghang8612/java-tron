5cf85957b8546b19e649fe78e003f735ef7e1614
Asuka <yanghang8612@163.com>
2024-09-06 16:26:20 +0800

    func(stats): merge energy usage from other

--- chainbase/src/main/java/org/tron/core/capsule/ContractStateCapsule.java
+++ chainbase/src/main/java/org/tron/core/capsule/ContractStateCapsule.java
@@ -452,7 +452,7 @@ public class ContractStateCapsule implements ProtoCapsule<ContractState> {
     if (other == null) {
       return;
     }
-    this.setEnergyUsage(other.getEnergyUsage());
+    this.addEnergyUsage(other.getEnergyUsage());
     this.setEnergyFactor(other.getEnergyFactor());
     this.setUpdateCycle(other.getUpdateCycle());
     this.addEnergyUsageTotal(other.getEnergyUsageTotal());
