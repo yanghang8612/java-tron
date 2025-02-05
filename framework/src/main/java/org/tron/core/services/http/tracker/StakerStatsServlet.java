39a9e697bbfd610c633460f503544839300e0f87
Asuka <yanghang8612@163.com>
2025-02-05 16:19:33 +0800

    func(stats): more option for staker stats query

--- framework/src/main/java/org/tron/core/services/http/tracker/StakerStatsServlet.java
+++ framework/src/main/java/org/tron/core/services/http/tracker/StakerStatsServlet.java
@@ -1,11 +1,16 @@
 package org.tron.core.services.http.tracker;
 
+import com.google.protobuf.ByteString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.tron.common.utils.Commons;
 import org.tron.common.utils.StringUtil;
+import org.tron.consensus.dpos.StateManager;
 import org.tron.protos.Protocol;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Component
@@ -15,19 +20,39 @@ public class StakerStatsServlet extends BaseTrackerServlet {
     @Override
     void responseGet() throws IOException, MissingParameterException {
         List<Protocol.StakerStat> stats = dps.getStakerStat(cycleNumber);
-        response.getWriter().println("Staker stats:");
-        for (Protocol.StakerStat stat : stats) {
-            response.getWriter().printf("%s: %d, %d%n",
-                StringUtil.encode58Check(stat.getAddress().toByteArray()),
-                stat.getStakedTrxForEnergy(),
-                stat.getMeu());
-            for (Protocol.StakerStat.DelegateStat delegate : stat.getDelegateStatsList()) {
-                response.getWriter().printf(" |- %s: %d, %d%n",
-                    StringUtil.encode58Check(delegate.getTo().toByteArray()),
-                    delegate.getAmount(),
-                    delegate.getMeu());
+        stats.sort(Comparator.comparingLong(Protocol.StakerStat::getStakedTrxForEnergy).reversed());
+
+        String addressStr = mayGetParameter("address", "");
+        if (addressStr.isEmpty()) {
+            for (Protocol.StakerStat stat : stats) {
+                response.getWriter().printf("%s %d %d %d%n",
+                    StringUtil.encode58Check(stat.getAddress().toByteArray()),
+                    stat.getStakedTrxForEnergy(),
+                    stat.getMeu(),
+                    stat.getDelegateStatsCount());
             }
-            response.getWriter().println();
+        } else {
+            byte[] address = Commons.decode58Check(addressStr);
+            if (address != null) {
+                String thresholdStr = mayGetParameter("threshold", "0");
+                long threshold = Long.parseLong(thresholdStr);
+                for (Protocol.StakerStat stat : stats) {
+                    if (stat.getAddress().equals(ByteString.copyFrom(address))) {
+                        List<Protocol.StakerStat.DelegateStat> delegates = new ArrayList<>(stat.getDelegateStatsList());
+                        delegates.sort(Comparator.comparingLong(Protocol.StakerStat.DelegateStat::getAmount).reversed());
+                        for (Protocol.StakerStat.DelegateStat delegate : delegates) {
+                            if (delegate.getAmount() >= threshold) {
+                                response.getWriter().printf("%s %d %d%n",
+                                    StringUtil.encode58Check(delegate.getTo().toByteArray()),
+                                    delegate.getAmount(),
+                                    delegate.getMeu());
+                            }
+                        }
+                        return;
+                    }
+                }
+                response.getWriter().println("Not found");
+            }
         }
     }
 }
