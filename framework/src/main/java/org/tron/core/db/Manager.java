2001623b4f1506058c87134d61cb8bf1442a3b78
shenchuanqiang <bill.shen@tron.network>
2025-11-21 17:13:17 +0800

    closeSelf. for exception. /fullnode

--- framework/src/main/java/org/tron/core/db/Manager.java
+++ framework/src/main/java/org/tron/core/db/Manager.java
@@ -1458,6 +1458,7 @@ public class Manager {
     } catch (Exception e) {
       logger.error("Block trigger failed. head: {}, oldSolid: {}, newSolid: {}",
           block.getNum(), oldSolid, newSolid, e);
+      ApplicationHandler.closeSelf();
       throw new TronError(e, TronError.ErrCode.EVENT_SUBSCRIBE_ERROR);
     }
   }
