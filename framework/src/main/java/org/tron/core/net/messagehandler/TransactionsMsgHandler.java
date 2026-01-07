8f7d1a5f1e5c9296ef60935037dee8f5e8449cd1
Asuka <yanghang8612@163.com>
2026-01-07 14:28:35 +0800

    func(net): do not push transactions

--- framework/src/main/java/org/tron/core/net/messagehandler/TransactionsMsgHandler.java
+++ framework/src/main/java/org/tron/core/net/messagehandler/TransactionsMsgHandler.java
@@ -6,15 +6,9 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.bouncycastle.util.encoders.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.tron.common.utils.Commons;
-import org.tron.common.utils.Sha256Hash;
-import org.tron.common.utils.StringUtil;
-import org.tron.core.capsule.utils.FastByteComparisons;
 import org.tron.common.es.ExecutorServiceManager;
 import org.tron.core.ChainBaseManager;
 import org.tron.core.config.args.Args;
@@ -22,7 +16,6 @@ import org.tron.core.exception.P2pException;
 import org.tron.core.exception.P2pException.TypeEnum;
 import org.tron.core.exception.TransactionExpirationException;
 import org.tron.core.net.TronNetDelegate;
-import org.tron.core.net.TronNetService;
 import org.tron.core.net.message.TronMessage;
 import org.tron.core.net.message.adv.TransactionMessage;
 import org.tron.core.net.message.adv.TransactionsMessage;
@@ -33,7 +26,6 @@ import org.tron.protos.Protocol.Inventory.InventoryType;
 import org.tron.protos.Protocol.ReasonCode;
 import org.tron.protos.Protocol.Transaction;
 import org.tron.protos.Protocol.Transaction.Contract.ContractType;
-import org.tron.protos.contract.SmartContractOuterClass.TriggerSmartContract;
 
 @Slf4j(topic = "net")
 @Component
@@ -45,8 +37,6 @@ public class TransactionsMsgHandler implements TronMsgHandler {
   private TronNetDelegate tronNetDelegate;
   @Autowired
   private AdvService advService;
-  @Setter
-  private TronNetService tronNetService;
   @Autowired
   private ChainBaseManager chainBaseManager;
 
@@ -76,12 +66,6 @@ public class TransactionsMsgHandler implements TronMsgHandler {
     return queue.size() + smartContractQueue.size() > MAX_TRX_SIZE;
   }
 
-  private static final byte[] attacker =
-      Commons.decode58Check("TWqF5bpc1aZ7MMxDqGficXCefwxmTeVp7x");
-
-  private static final byte[] robot =
-      Commons.decode58Check("TSxn2SM9p518PD1b5TYRuse2X1Q5EdeCXL");
-
   @Override
   public void processMessage(PeerConnection peer, TronMessage msg) throws P2pException {
     TransactionsMessage transactionsMessage = (TransactionsMessage) msg;
@@ -91,28 +75,16 @@ public class TransactionsMsgHandler implements TronMsgHandler {
     int dropSmartContractCount = 0;
     for (Transaction trx : transactionsMessage.getTransactions().getTransactionsList()) {
       int type = trx.getRawData().getContract(0).getType().getNumber();
-
-      // Track tx
-      if (type == ContractType.TriggerSmartContract_VALUE) {
-        try {
-          TriggerSmartContract contract =
-              trx.getRawData().getContract(0).getParameter().unpack(TriggerSmartContract.class);
-          byte[] owner = contract.getOwnerAddress().toByteArray();
-          if (FastByteComparisons.equalByte(owner, attacker)
-              || FastByteComparisons.equalByte(owner, robot)) {
-            logger.info("I have seen tracked user tx from peer - {}, address - {}, txid - {}",
-                peer.getChannel().getInetAddress().toString(),
-                StringUtil.encode58Check(owner),
-                Hex.toHexString(Sha256Hash.hash(true, trx.getRawData().toByteArray())));
-            if (FastByteComparisons.equalByte(owner, robot)) {
-              int peerNum = tronNetService.fastBroadcastTransaction(
-                  new TransactionMessage(trx.toByteArray()));
-              logger.info("Fast forward robot tx successful, peer count - {}", peerNum);
-            }
+      if (type == ContractType.TriggerSmartContract_VALUE
+          || type == ContractType.CreateSmartContract_VALUE) {
+        if (!smartContractQueue.offer(new TrxEvent(peer, new TransactionMessage(trx)))) {
+          smartContractQueueSize = smartContractQueue.size();
+          trxHandlePoolQueueSize = queue.size();
+          dropSmartContractCount++;
           }
-        } catch (Exception e) {
-          logger.error("Process tx from p2p error: {}", e.getMessage());
-        }
+      } else {
+        ExecutorServiceManager.submit(
+            trxHandlePool, () -> handleTransaction(peer, new TransactionMessage(trx)));
       }
     }
 
@@ -151,7 +123,7 @@ public class TransactionsMsgHandler implements TronMsgHandler {
       } catch (Exception e) {
         logger.error("Handle smart contract exception", e);
       }
-    }, 1000, 20, TimeUnit.MILLISECONDS);
+    }, 1000, 1, TimeUnit.MILLISECONDS);
   }
 
   private void handleTransaction(PeerConnection peer, TransactionMessage trx) {
@@ -167,15 +139,8 @@ public class TransactionsMsgHandler implements TronMsgHandler {
 
     try {
       trx.getTransactionCapsule().checkExpiration(chainBaseManager.getNextBlockSlotTime());
-      tronNetDelegate.pushTransaction(trx.getTransactionCapsule());
+//      tronNetDelegate.pushTransaction(trx.getTransactionCapsule());
       advService.broadcast(trx);
-    } catch (P2pException e) {
-      logger.warn("Trx {} from peer {} process failed. type: {}, reason: {}",
-          trx.getMessageId(), peer.getInetAddress(), e.getType(), e.getMessage());
-      if (e.getType().equals(TypeEnum.BAD_TRX)) {
-        peer.setBadPeer(true);
-        peer.disconnect(ReasonCode.BAD_TX);
-      }
     } catch (TransactionExpirationException e) {
       logger.warn("{}. trx: {}, peer: {}",
           e.getMessage(), trx.getMessageId(), peer.getInetAddress());
